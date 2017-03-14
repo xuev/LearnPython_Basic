e030f87f50e4723e6df47d4abebc6e3cd80d9ab4
xuev <xuewei918@gmail.com>
2017-03-15 03:25:53 +0800

    W01V07 Modified

--- W01/V07/TempConvert.py
+++ W01/V07/TempConvert.py
@@ -11,3 +11,19 @@
 @Description: 
 """
 
+
+def fc(val):
+    val = input("请输入带温度标识符号的温度值（例如：32C）： ")
+    if val[-1] in ['C', 'c']:
+        f = 1.8 * float(val[0:-1]) + 32
+        print("转换后的温度为： %0.2f°F" % f)
+        return fc(val)
+    elif val[-1] in ['F', 'f']:
+        c = (float(val[0:-1]) - 32) / 1.8
+        print("转换后的温度为： %0.2f°C" % c)
+        return fc(val)
+    else:
+        print("输入有误，请重新输入！")
+        return fc(val)
+
+a = fc(input())
