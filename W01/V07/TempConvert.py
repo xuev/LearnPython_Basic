20fffee71da62012c764a4e189e1bbb3df6c5251
xuev <xuewei918@gmail.com>
2017-03-15 03:25:17 +0800

    W01V07

---
+++ W01/V07/TempConvert.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author:  xuev (xuewei918@gmail.com)
+@Project: LearnPython_Basic
+@File: TempConvert.py
+@Version: 0.01
+@License: MIT Licence
+@Create Time: 2017/3/15 02:50
+@Description: 
+"""
+
