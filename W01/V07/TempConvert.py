b9994dc8bc64c7a743f569bcb7055cfa6593deb9
xuev <xuewei918@gmail.com>
2017-03-15 04:15:09 +0800

    Improve process on w01v07

--- W01/V07/TempConvert.py
+++ W01/V07/TempConvert.py
@@ -13,17 +13,37 @@
 
 
 def fc(val):
-    val = input("请输入带温度标识符号的温度值（例如：32C）： ")
-    if val[-1] in ['C', 'c']:
-        f = 1.8 * float(val[0:-1]) + 32
-        print("转换后的温度为： %0.2f°F" % f)
-        return fc(val)
-    elif val[-1] in ['F', 'f']:
-        c = (float(val[0:-1]) - 32) / 1.8
-        print("转换后的温度为： %0.2f°C" % c)
-        return fc(val)
+    print("-" * 50)
+    val = input("请输入带温度标识符号的温度值（例如：24C 或 100F）： ")
+    if len(val) != 0:
+        if val[-1] in ['C', 'c']:
+            for m in val[0:-1]:
+                if m in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
+                    f = 1.8 * float(val[0:-1]) + 32
+                    print()
+                    print("    您输入的是 " + str(float(val[0:-1])) + " °C，转换后的温度为： %0.2f °F" % f)
+                    print()
+                    return fc(val)
+            print()
+            print("    输入有误，请重新输入！")
+            print()
+            return fc(val)
+        elif val[-1] in ['F', 'f']:
+            for n in val[0:-1]:
+                if n in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
+                    c = (float(val[0:-1]) - 32) / 1.8
+                    print()
+                    print("    您输入的是 " + str(float(val[0:-1])) + " °F，转换后的温度为： %0.2f °C" % c)
+                    print()
+                    return fc(val)
+            print()
+            print("    输入有误，请重新输入！")
+            print()
+            return fc(val)
     else:
-        print("输入有误，请重新输入！")
+        print()
+        print("    输入有误，请重新输入！")
+        print()
         return fc(val)
 
 a = fc(input())
